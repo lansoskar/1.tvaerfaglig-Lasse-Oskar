844750e3dc643dc1640800c51cb1ae1cdb5a0716
daugbjerg <daugbjerglasse@gmail.com>
2023-04-20 09:54:08 +0200

    Arraylist local funktionalitet.

--- app/src/main/java/com/example/indkoebsliste/MainActivity.java
+++ app/src/main/java/com/example/indkoebsliste/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.indkoebsliste;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_hverdagslisten);
+        Vare vare = new Vare("Mælk", false);
+        Hverdagslisten hverdagslisten = new Hverdagslisten();
+        hverdagslisten.addVare(vare);
+        System.out.println(hverdagslisten);
     }
-}
+    }
+
+
+/* private FirebaseFirestore db = FirebaseFirestore.getInstance(); */
